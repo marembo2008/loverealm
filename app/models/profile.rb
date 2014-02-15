87a5041f92f792d22c91e7b1f61f2dd2228bc453
Marembo Ochieng <marembo2008@gmail.com>
2014-02-15 23:52:50 +0300

    profile info and credential entities, plus relevant active record validations

---
+++ app/models/profile.rb
@@ -0,0 +1,9 @@
+class Profile < ActiveRecord::Base
+ attr_accessor :firstName, :lastName, :sex, :dob, :location, :bio
+
+ #we need to have all attributes except bio.
+ validates :firstName, presence: true
+ validates :lastName. presence: true
+ validates :sex, presence: true, inclusion: {in: %w{male, female}, message: "%{value} is not a sex option"}
+ validates :dob, presence: true
+end
