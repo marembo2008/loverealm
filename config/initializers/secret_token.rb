6a4353375f80ee328a7b7da1099b5d9220921078
Marembo Ochieng <marembo2008@gmail.com>
2014-02-15 19:50:00 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Loverealm::Application.config.secret_key_base = 'd73f235049b61b1e26c615ce37ebc642ba0af6c5098067dbcb1b02294a7525f90de87bbe416e91d7372901236888fecdaecc4aea68ff25fbfc65d796db095c81'
