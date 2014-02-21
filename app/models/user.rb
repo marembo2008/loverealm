117cb7399400bcfbc5e9ab154aca115e69cc139a
Marembo Ochieng <marembo2008@gmail.com>
2014-02-21 15:51:41 +0300

    removing my own entity, kenn's preference

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :fname,:lname, :country, :birthday, :email, 
-                  :password,  :password_confirmation,:avatar, 
+  attr_accessible :fname,:lname, :country, :birthday, :email,
+                  :password,  :password_confirmation,:avatar,
                   :validated, :validation_code,
                   :emailsecret, :secretcode
   has_secure_password
 
   mount_uploader :avatar, AvatarUploader
-before_save do |user|
-	require 'digest/md5'
+  before_save do |user|
+    require 'digest/md5'
     user.email = email.downcase
     user.username = user.email
     user.username = user.username.gsub(/(@.+)/, "")
