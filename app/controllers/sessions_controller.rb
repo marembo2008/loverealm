5be1c7991469970d77c8ede6d0e205f0eee64917
kenn <kennmunene@gmail.com>
2014-03-03 22:06:08 +0300

    fb login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController <  ApplicationController
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
+    authorization = Authorization.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = authorization.uid
     redirect_to root_url
   end
 
