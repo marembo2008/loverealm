e5b65e5ca0dd4837c94ca45ef16d70f973d0195c
Marembo Ochieng <marembo2008@gmail.com>
2014-03-20 19:14:34 +0300

    myaccount design complete

---
+++ app/assets/javascripts/profile-main.js
@@ -0,0 +1,35 @@
+$(function() {
+	$('div.main .ct.profile .dh .dheader a').click(function() {
+		//get the href
+		var href = $(this).data('href');
+		$('div.main .ct.profile .dh .dheader a').removeClass('active-option');
+		$(this).addClass('active-option');
+		var activeContent = 'div.main .ct.profile .dh .dcontent .dcontent-options.' + href;
+		console.log('active content: ' + activeContent);
+		//hide all contents.
+		$('div.main .ct.profile .dh .dcontent .dcontent-options').hide();
+		$(activeContent).show();
+		return false;
+	});
+
+	$('a#add-account').click(function() {
+		var active = $(this).data('active');
+		console.log('active=' + active);
+		if (active === 'false') {
+			//show status update
+			$('div#status-update-panel').slideDown(1000, function() {
+				$(this).show();
+				$('span#status-update').show();
+			});
+			$(this).data('active', 'true');
+		} else {
+			//hide status update
+			$('div#status-update-panel').slideUp(1000, function() {
+				$(this).hide();
+				$('span#status-update').hide();
+			});
+			$(this).data('active', 'false');
+		}
+		return false;
+	});
+});
